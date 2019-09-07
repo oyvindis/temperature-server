231ac5f0ee3f53d7f5bddf905841e42e33aa0541
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-09-07 13:09:36 +0200

    refactor: start server

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 
-const app = express();
-const port = 3333;
-app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
-});
-app.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
-    return console.log(`server is listening on ${port}`);
-});
+export const server = {
+    start(): void {
+        const app = express();
+        const port = 3333;
+        app.get('/', (req, res) => {
+            res.send('The sedulous hyena ate the antelope!');
+        });
+        app.listen(port, err => {
+            if (err) {
+                return console.error(err);
+            }
+            return console.log(`server is listening on ${port}`);
+        });
+    },
+};
