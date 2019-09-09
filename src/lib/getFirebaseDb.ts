0eab6bf4666523591a65a723e9951739df32e6ff
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-09-09 19:28:55 +0200

    fix: firebase initialize

--- src/lib/getFirebaseDb.ts
+++ src/lib/getFirebaseDb.ts
@@ -1,10 +1,16 @@
 import firebaseAdmin, { database } from 'firebase-admin';
-
-const credentials: firebaseAdmin.ServiceAccount = process.env.FIREBASE_TEMPERATURE_CREDENTIALS
-    ? JSON.parse(process.env.FIREBASE_TEMPERATURE_CREDENTIALS)
-    : undefined;
+import { Base64 } from 'js-base64';
+import get from 'lodash/get';
 
 const FIREBASE_BASE_URL = 'https://api-temperature.firebaseio.com/';
+
+const FIREBASE_TEMPERATURE_CREDENTIALS = process.env.FIREBASE_TEMPERATURE_CREDENTIALS;
+
+const FIREBASE_TEMPERATURE_CREDENTIALS_KEY = get(FIREBASE_TEMPERATURE_CREDENTIALS, ['data', 'key.json']);
+
+const credentials: firebaseAdmin.ServiceAccount = FIREBASE_TEMPERATURE_CREDENTIALS_KEY
+    ? JSON.parse(Base64.decode(FIREBASE_TEMPERATURE_CREDENTIALS_KEY))
+    : undefined;
 
 if (credentials) {
     firebaseAdmin.initializeApp({
